da2441154ffe96499dce8cb6f92469c46951a71b
Aditi Mohanty <aditi.anomita@gmail.com>
2016-04-25 19:00:18 +0530

    f

--- angular-votr/server.js
+++ angular-votr/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.listen(1337, function(){
-	console.log("Server running on port 1337");
-})
+
 
 app.use(express.static(__dirname + "/public"));
+
+app.listen(1337, function(){
+	console.log("Server running on port 1337");
+});
