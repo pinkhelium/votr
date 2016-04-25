e1bc1cad10d2e39b19950f871fad3158607f4a01
Aditi Mohanty <aditi.anomita@gmail.com>
2016-04-25 20:09:55 +0530

    login partial

--- angular-votr/server.js
+++ angular-votr/server.js
@@ -1,10 +1,85 @@
 var express = require('express');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('connect-flash');
+
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+
+var CONFIG = require('./config.js');
+var C = {};
+
 var app = express();
-
-
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + "/public"));
+app.use(cookieParser());
+app.use(session({
+	secret: "YOLOVOTR",
+	cookie: { maxAge: 60000 }
+}));
+app.use(flash());
 
-app.listen(1337, function(){
-	console.log("Server running on port 1337");
+
+
+// PASSPORT RELATED CONFIGS
+passport.use(new Strategy({
+    clientID: CONFIG.clientID,
+    clientSecret: CONFIG.clientSecret,
+    callbackURL: CONFIG.callbackUrl
+  },
+  // the VERIFY callback
+  function(accessToken, refreshToken, profile, done) {
+    console.log("\n\n\nVERIFY CALLBACK:\nAccessToken:" + accessToken);
+
+    // graph.setAccessToken(accessToken);
+    C.ACCESS_TOKEN = accessToken;
+    // graph.setAppSecret(CONFIG.graphAppSecret);
+    return done(null, profile);
+  }));
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+
+app.get('/login', passport.authenticate('facebook'));
+
+app.get(
+	'/login/facebook/return',
+	passport.authenticate('facebook', {
+		successRedirect: '/loginsuccess',
+		failureRedirect: '/loginfailure',
+		failureFlash: 'Failed to login',
+		successFlash: 'Login Success! Welcome'
+	})
+);
+
+app.get('/loginsuccess', function(request, response){
+	console.log("IN LOGIN SUCCESS CALL");
+	console.log(request.user);
+	response.send("SUCCESS");
+});
+
+app.get('/loginfailure', function(request, response){
+	response.send("LOGINFAIL");
+});
+
+app.get('/randomshit', function(request, response){
+	console.log('HI');
+	console.log(request.user);
+	response.send('lets see' + request.user);
+});
+
+app.listen(port, function(){
+	console.log("Server running on port: " + port);
 });
