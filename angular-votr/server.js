74eebc35215e85971fccbab8dd77657c0cbbde21
Abinav Seelan <abinav.n.seelan@gmail.com>
2016-04-25 20:08:31 +0530

    Setting up Controllers

--- angular-votr/server.js
+++ angular-votr/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+
+app.get('api/sec/', function(request, response){
+
+});
+
+
+app.get('/api', function(request, response){
+
+});
+
+
 app.listen(1337, function(){
 	console.log("Server running on port 1337");
-})
+});
 
-app.use(express.static(__dirname + "/public"));
