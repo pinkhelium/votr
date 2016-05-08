80357a800da969b743f6de20215ccb50ac3504ca
cyberbeast <sandeshgade@gmail.com>
2016-05-08 21:27:01 +0530

    UI2.0 Major Updates

--- public/js/main.js
+++ public/js/main.js
@@ -664,3 +664,17 @@ app.controller('CandidatesCtrl', function($http,$scope,$q,$route){
 app.controller('NominationResultsCtrl', function($http,$scope,$q){
 
 });
+
+app.controller('TabCtrl', [function() {
+    angular.element(document).ready(function () {
+        $('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
+
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
+    });
+}]);
